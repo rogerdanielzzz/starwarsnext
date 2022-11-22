1b4121f8908cbcebf19e2b896aea6eeb6ac3cd42
Roger Perez <roger.perezcol@gmail.com>
2022-11-22 19:42:42 -0300

    first commit

--- comps/NavBar.jsx
+++ comps/NavBar.jsx
@@ -8,7 +8,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 // Components made with React
-import Searchbar from '../Searchbar/Searchbar';
+import Searchbar from './Searchbar';
 // Style in SCSS format
 import Style from "./NavBar.module.scss"
 //Img import
