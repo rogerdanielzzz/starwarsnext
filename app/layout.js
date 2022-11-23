afe7b1cd86fe6100c06ad9388d1ee4b07b03b028
Roger Perez <roger.perezcol@gmail.com>
2022-11-23 18:26:40 -0300

    Meta tag

--- app/layout.js
+++ app/layout.js
@@ -10,13 +10,18 @@ export default function RootLayout({ children }) {
   return (
     <Provider store={store}>
 
-    <html>
+    <html lang="en">
       <head>
+      <meta
+      name="description"
+      content="A little project for a Selection Challenge made it by Roger Pérez"
+    />
+    <meta charset="utf-8" />
       <meta  property="og:title" content="Star Wars Character App "/>
       <meta property="og:image" content="https://i.ebayimg.com/images/g/tMkAAOSwRnNdYnE7/s-l500.jpg"/>
       <meta property="og:description"
     content="A little project for a Selection Challenge made it by Roger Pérez"/>
-    <title> Star Wars</title>
+    <title> Star Wars | characters</title>
 
       </head>
 
